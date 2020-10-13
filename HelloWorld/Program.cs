83fbd1feb1bffb0fff0f505f74fd1a0fce0b8c29
Victor Illera <illera88@hotmail.com>
2020-10-13 20:59:40 +0200

    Pujada inicial

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola món");
+            Console.WriteLine("Versió 0.1 del projecte prjava00");
+            Console.ReadLine();
+        }
+    }
+}

e64cc21d83387f90bee967bb3002a0da2ee6ab7b
Victor Illera <illera88@hotmail.com>
2020-10-14 20:06:51 +0200

    “Versió 0.2 del projecte prjava00”

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace HelloWorld
@@ -10,9 +11,29 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+
+            // TODO code application logic here
             Console.WriteLine("Hola món");
-            Console.WriteLine("Versió 0.1 del projecte prjava00");
-            Console.ReadLine();
+            Console.WriteLine("versió 0.2 del projecte prjava00");
+            try
+            {
+  
+               
+                String hostname = Dns.GetHostName();
+                IPAddress[] ipAddr = Dns.GetHostAddresses(hostname);
+                Console.WriteLine("hostname=" + hostname);
+                Console.WriteLine("Adreça IP: " + ipAddr); 
+                Console.WriteLine("Nom de l'usuari: user.name");
+                Console.WriteLine("Carpeta Personal: user.home");
+                Console.WriteLine("Sistema operatiu: os.name");
+                Console.WriteLine("Versió OS:os.version");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
